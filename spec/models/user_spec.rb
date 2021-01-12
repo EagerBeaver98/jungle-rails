1ffdc8f57f393ba32eadb8e213dcf5cdd2f3fa74
John <jooke98@outlook.com>
2021-01-12 14:28:16 -0700

    user validation working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "Validations" do
-    it ''
+    it 'creates user obect' do
+      @user = User.new
+      expect(@user).to be_present
+    end
+    it 'must have a password' do
+      @user = User.new(password: 'bacon', password_confirmation: 'bacon')
+      expect(@user.password).to be_present
+    end
+    it 'must have unique email address' do
+      @user1 = User.new(email: 'test@test.com')
+      @user2 = User.new(email: 'test@test.com')
+      expect().to be_nil
+    end
+    
   end
 end
